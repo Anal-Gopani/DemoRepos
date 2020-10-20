c078dac460a22c8b6cc83b07a9fde84fe5ebbf78
analg <analg@LAPTOP-AK2RNQI9>
2020-10-20 17:54:20 +0530

    Add project files.

---
+++ demo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        
+        public ActionResult Index()
+        {
+            List<student> std = new List<student>
+            {
+                new student
+                {
+                    city = "surat",
+                    number = 1,
+                    name = "nanu"
+                },
+                new student
+                {
+                    city = "bhavnagar",
+                    number = 2,
+                    name = "gopi"
+                },
+                
+            };
+            //ViewData["Message"] = std;
+             return View(std);
+            
+        }
+        public ActionResult create()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ demo/Models/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo.Models
+{
+    public class student
+    {
+        public int number { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+    }
+}
